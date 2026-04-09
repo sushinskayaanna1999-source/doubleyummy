15ff18af1238001f1fbccd9ada6edcc10b8bcc6d
sushinskayaanna1999-source <sushinskaya.anna1999@gmail.com>
2026-04-09 23:41:19 +0300

    remove pwa register

--- doubleyummy-web/src/main.tsx
+++ doubleyummy-web/src/main.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { registerSW } from "virtual:pwa-register";
 import { App } from "./App";
 import "./styles/globals.css";
 
 const queryClient = new QueryClient();
-registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
